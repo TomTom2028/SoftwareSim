77b61e5acff78b2842d1e3c7c557509831cb9e5a
Robbe Hovaert <robbe.hovaert@gmail.com>
2025-02-20 11:35:18 +0100

    basic thing completly working

--- simpleLift.py
+++ simpleLift.py
@@ -1,5 +1,11 @@
 # simple shit: one floor that spawns items. the lift takes the tray down, and takes one of the tray
 import salabim as sim
+
+class Person(sim.Component):
+    def get_picktime(self):
+        return sim.Poisson(20).sample()
+
+
 class OrderGenerator(sim.Component):
     def process(self):
         while True:
@@ -17,12 +23,13 @@ class Order(sim.Component):
 
 #lift takes order out of the queue
 class Lift(sim.Component):
-    def __init__(self, target_floor_number, speed, pickup_time):
+    def __init__(self, target_floor_number, speed, loading_time, picker):
         super().__init__()
         self.target_floor_number = target_floor_number
         self.current_floor_number = target_floor_number
-        self.pickup_time = pickup_time
+        self.loading_time = loading_time
         self.speed = self.speed = speed
+        self.picker = picker;
 
     def process(self):
         while True:
@@ -35,13 +42,18 @@ class Lift(sim.Component):
             self.current_floor_number = current_order.floor_number
             #TODO: actually pickup
             # we "pick up" the thing
-            self.hold(30) # takeout time
+            self.hold(self.loading_time) # robot loading time
+            self.hold(hold_time)
+            self.current_floor_number = 0 # back to zero
+            self.hold(self.picker.get_picktime)
+
 
 
 
 env = sim.Environment(trace=True)
 orderQueue = sim.Queue('orderQueue')
-Lift(0)
+person = Person()
+Lift(0, 1, 10, person)
 OrderGenerator()
 
 env.run(till=5000)
